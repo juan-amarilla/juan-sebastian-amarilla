dbe980da2bcf201e5a117efb4c7af1c40bbc549b
frc95 <frc95zz@gmail.com>
2021-02-01 19:16:14 -0300

    mi primer commit

---
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -0,0 +1,12 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    let mensaje;
+
+    mensaje = prompt("Por favor ingrese el mensaje: ");
+
+	alert(mensaje);
+}
+
